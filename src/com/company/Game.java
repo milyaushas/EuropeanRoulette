8abcfffe1a5ecd6448094707d0c88fad1c695939
Milyausha Sabirova <milyaushasab@gmail.com>
2022-11-30 14:25:10 +0300

    Add multiplayer regime and refactor code and tests

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,61 +4,70 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Game {
-    public Player player;
+    public final int players_num;
+    public Player[] players;
 
-    public Game() {
-        player = new Player();
+    public Game(int players_num) {
+        this.players_num = players_num;
+        this.players = new Player[players_num];
+        for (int i = 0; i < players_num; i++){
+            String name = "Игрок " + Integer.toString(i + 1);
+            this.players[i] = new Player(name);
+        }
     }
 
     public Game(Player player) {
-        this.player = player;
+        players_num = 1;
+        players = new Player[1];
+        players[0] = player;
     }
 
     public void start() throws IOException {
-        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < players_num; i++) {
+            Scanner in = new Scanner(System.in);
+            Player player = players[i];
 
-        System.out.println("У вас доступно " + player.score + ". Введите ставку");
-        this.player.bet = in.nextInt();
+            System.out.println(player.name + ", у вас есть " + player.score + " очков. Введите ставку");
+            player.bet = in.nextInt();
 
-        System.out.println("Введите -1, если будете ставить на цвет, 1 -- если на число");
-        this.player.field = in.nextInt();
+            System.out.println("Введите -1, если будете ставить на цвет, 1 -- если на число");
+            player.field = in.nextInt();
 
-        if (player.field < 0) {
-            System.out.println("Введите 1, если будете ставить на красный, 2 -- если на чёрный");
-        }
-        else {
-            System.out.println("Выберете число от 0 до 36");
-        }
+            if (player.field < 0) {
+                System.out.println("Введите 1, если будете ставить на красный, 2 -- если на чёрный");
+            } else {
+                System.out.println("Выберете число от 0 до 36");
+            }
 
-        this.player.field *= in.nextInt();
-        if (result(rotateRoulette())) {
-            System.out.print("Победа!");
-            updateScore(true);
-        }
-        else {
-            updateScore(false);
-            System.out.print("Поражение :(");
+            player.field *= in.nextInt();
+            if (result(i, rotateRoulette())) {
+                System.out.print("Победа!\n\n\n");
+                updateScore(i, true);
+            } else {
+                updateScore(i, false);
+                System.out.print("Поражение :(\n\n\n");
+            }
         }
     }
 
-    public void updateScore(boolean isWin) {
+    public void updateScore(int player_ind, boolean isWin) {
         if (isWin) {
-            if (player.field < 0) {
-                player.score += player.bet;
+            if (players[player_ind].field < 0) {
+                players[player_ind].score += players[player_ind].bet;
             } else {
-                player.score += player.bet * 36;
+                players[player_ind].score += players[player_ind].bet * 36;
             }
         }
         else {
-            player.score -= player.bet;
+            players[player_ind].score -= players[player_ind].bet;
         }
     }
 
-    public boolean result(int resultRotate) {
-        if (player.field < 0) {
-            return (-player.field) % 2 == resultRotate % 2;
+    public boolean result(int player_ind, int resultRotate) {
+        if (players[player_ind].field < 0) {
+            return (-players[player_ind].field) % 2 == resultRotate % 2;
         }
-        return this.player.field == resultRotate;
+        return this.players[player_ind].field == resultRotate;
     }
 
     public static int rotateRoulette() {
